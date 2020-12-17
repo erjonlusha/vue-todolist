389ae46ba0a3dd84886e07dfba354ec359faca62
Erjon <erjonlusha@gmail.com>
2020-12-17 01:09:25 +0100

    Creare una todo list usando VueJS.

---
+++ assets/js/main.js
@@ -0,0 +1,25 @@
+let app = new Vue({
+    el:'#app'
+    data : {
+        toDo: [
+            "Fotocopire",
+            "Spesa",
+            "Facende domestiche"
+        ]
+    },
+    // created() {
+    //     document.addEventListener("keyup", (e) => {
+    //       if (e.key === invio o click bottone) {
+    //         aggiungere item
+    //       }
+    //     });
+    // },      
+    // methods : {
+    //     add...(){
+    //         .push();
+    //     },
+    //     remove(index){
+    //         .splice(index, 1);
+    //     }
+    // }
+});
