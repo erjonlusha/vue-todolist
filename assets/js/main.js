d52604ccdba569c3474b4576d6106f4c0f826abb
Erjon <erjonlusha@gmail.com>
2020-12-17 18:31:10 +0100

    Creata to do list
    aggiunge e rimuove le task

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,43 @@
+/*vue-todolist
+// Istruzioni:
+// Create una todo list usando VueJS.
+// Potete dare sfogo alla creativitá e per quanto riguarda l'HTML e il CSS.
+// Se non sapere che fare, di seguito trovate uno screenshot.
+
+Funzionalitá:
+// La nostra todo list avrá alcune tasks di default predefinite
+// L'utente puó inserire nuove tasks
+// Cliccando sulla "X" l'utente puó cancellare una task
+// Se non ci sono piu task nella lista, mostrate un messaggio tipo "Nulla da fare"
+// Quando l'utente inserisce una task ha due modi per salvarla: o preme il pulsante add o preme il taso Enter della tastiera.
+// Attenzione: l'utente non deve inserire tasks vuote ma almeno un tot di caratteri.
+
+Da tenere a mente:
+Ricordate di scrivere le istruzioni prima di iniziare a lavorare cercate di descrivere in modo sintetico gli step che pensate siano necessari.
+Consultate la documentazione e se siete bloccati, aprite un ticket.*/
+
 let app = new Vue({
-    el:'#app'
+    el:'#app',
     data : {
         toDo: [
-            "Fotocopire",
+            "Fotocopie",
             "Spesa",
-            "Facende domestiche"
+            "Facende domestiche",
         ]
     },
-    // created() {
-    //     document.addEventListener("keyup", (e) => {
-    //       if (e.key === invio o click bottone) {
-    //         aggiungere item
-    //       }
-    //     });
-    // },      
-    // methods : {
-    //     add...(){
-    //         .push();
-    //     },
-    //     remove(index){
-    //         .splice(index, 1);
-    //     }
-    // }
+    methods : {
+        addToList(){
+        if (this.item.length>= 5) {
+            this.toDo.push(this.item);
+            this.item = "";
+        }else {
+            alert("Error!!! Aggiungi piu parole");
+        };
+        },
+        removeItem(index){
+            this.toDo.splice(index, 1);
+        },
+    }
 });
+
+
